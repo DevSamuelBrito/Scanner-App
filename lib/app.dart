a91ee597b17bf37d4755cd91daddda83a5330641
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-04-11 12:39:21 -0300

    create the default app

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScannerApp extends StatelessWidget {
+  const ScannerApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Scanner App",);
+  }
+}
