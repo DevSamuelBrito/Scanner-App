0a570d00ff2a628ffc4540b015c7e0cd57b3d4ef
Rafaelx32 <117119646+Rafaelx32@users.noreply.github.com>
2024-04-21 19:32:59 -0300

    Tentei fazer a tela de cadastro sem o firebase
    A página não está carregando está dando uma exceção de global key, não consegui identificar onde está a exceção

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:scanner_app/screens/cadastrarProdutos_page.dart';
 
 class ScannerApp extends StatelessWidget {
-  const ScannerApp({Key? key}) : super(key: key);
+  const ScannerApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Scanner App",);
+    return MaterialApp(
+      title: "Scanner App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(
+        useMaterial3: false,
+      ),
+      // Definindo as rotas para as telas do app
+      routes: {
+        "/cadastrarProdutos": (context) =>
+            CadastrarProdutosPage(), // rota para a tela cadastro produtos
+      },
+    );
   }
 }
---
+++ lib/screens/cadastrarProdutos_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class CadastrarProdutosPage extends StatelessWidget {
+  CadastrarProdutosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 240,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Descrição",
+                  ),
+                  keyboardType: TextInputType
+                      .emailAddress // Vai definir o teclado que vai aparecer no dispositivo,
+                  //maxLines: 5, //Número máximo de linhas para digitar
+                  ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Preço da compra",
+                ),
+
+                obscureText: true, // Vai mascarar a senha para ninguém ver
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Margem/Markup",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Lucro",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Preço de Venda",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Garantia",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Marca",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Referência",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Validade",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Comissão %",
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                    child: Text("Cadastrar"),
+                    onPressed: () {} // A chave para uma função anônima,
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ); // body é o restante em branco da tela
+  }
+}
