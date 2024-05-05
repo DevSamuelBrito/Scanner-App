6dad324890ab459c833fd11bc0046271214247a0
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-05-05 15:18:42 -0300

    update the resume screen

--- lib/screens/telaResumo.dart
+++ lib/screens/telaResumo.dart
@@ -1,11 +1,22 @@
+import 'dart:js';
 import 'package:flutter/material.dart';
 
 class TelaResumo extends StatelessWidget {
+  void returnHome(BuildContext context) {
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
   @override
-  Widget build(BuildContext buildContext) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Tela de Resumos"),
+        actions: [
+          IconButton(
+            onPressed: () => returnHome(context),
+            icon: Icon(Icons.logout),
+          ),
+        ],
       ),
     );
   }
