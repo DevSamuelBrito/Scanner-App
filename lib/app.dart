9dd6f44de33057f2ce989098a7825167ad6cbe99
Rafaelx32 <117119646+Rafaelx32@users.noreply.github.com>
2024-05-05 22:12:06 -0300

    Voltei a versão e tirei a opção de colocar imagem que estava quebrando o código

--- lib/app.dart
+++ lib/app.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:scanner_app/screens/cadastrarProdutos_page.dart';
 
 class ScannerApp extends StatelessWidget {
-  const ScannerApp({Key? key, this.title = "Scanner App"}) : super(key: key);
+  const ScannerApp({super.key});
 
-  final String title;
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
+      title: "Scanner App",
       debugShowCheckedModeBanner: false,
       initialRoute: "/cadastrarProdutos",
       // Definindo as rotas para as telas do app
       routes: {
         "/cadastrarProdutos": (context) => const CadastrarProdutosPage(
-              title: "Cadastro do Produto",
-            ), // rota para a tela cadastro produtos
+            title: "Cadastro do Produto"), // rota para a tela cadastro produtos
       },
     );
   }
--- lib/screens/cadastrarProdutos_page.dart
+++ lib/screens/cadastrarProdutos_page.dart
@@ -1,15 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:barcode_widget/barcode_widget.dart';
-import 'dart:math';
 import 'dart:io';
+import 'dart:math';
 
 class CadastrarProdutosPage extends StatefulWidget {
   const CadastrarProdutosPage({Key? key, required this.title})
@@ -23,17 +21,6 @@ class CadastrarProdutosPage extends StatefulWidget {
 
 class _CadastrarProdutosPageState extends State<CadastrarProdutosPage> {
   String randomNumbers = '';
-
-  File? imageFile;
-
-  final FirebaseStorage storage = FirebaseStorage.instance;
-  bool uploading = false;
-  double total = 0;
-
-  Future<XFile?> getImage() async {
-    final ImagePicker _picker = ImagePicker();
-    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-  }
 
   void _generatedRandomNumber() {
     setState(
@@ -55,115 +42,10 @@ class _CadastrarProdutosPageState extends State<CadastrarProdutosPage> {
         'descricao': txtDescricao.text,
         'precoVenda': txtPrecoVenda.text,
         'referencia': txtReferencia.text,
+        'codigoBarras': randomNumbers,
       },
     );
     Navigator.pop(context);
-  }
-
-  Future<UploadTask> _uploadImageToFirebaseStorage(String path) async {
-    File file = File(path);
-    try {
-      //Refêrencia ao local onde a imagem será armazenada no Firebase Storage
-      String ref = "images/img-${DateTime.now().toString()}.jpg";
-
-      //Upoload da imagem para o Firebase Storage
-      return storage.ref(ref).putFile(file);
-    } catch (e) {
-      // Em caso de erro, imprime o erro e retorna null
-      throw Exception('Erro no upload');
-    }
-  }
-
-  _pickAndUploadImage() async {
-    XFile? file = await getImage();
-    if (file != null) {
-      UploadTask task = await _uploadImageToFirebaseStorage(file.path);
-      task.snapshotEvents.listen((TaskSnapshot snapshot) async {
-        if (snapshot.state == TaskState.running) {
-          setState(() {
-            uploading = true;
-            total = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          });
-        } else if (snapshot.state == TaskState.success) {
-          setState(() => uploading = false);
-        }
-      });
-    }
-  }
-
-  void _ShowOpcoesBottomSheet() {
-    showModalBottomSheet(
-      context: context,
-      builder: (_) {
-        return Padding(
-          padding: const EdgeInsets.all(14),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[200],
-                  child: Center(
-                    child: Icon(
-                      PhosphorIcons.upload(PhosphorIconsStyle.regular),
-                    ),
-                  ),
-                ),
-                title: Text(
-                  'Galeria',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  // _pickAndUploadImage();
-                },
-              ),
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[200],
-                  child: Center(
-                    child: Icon(
-                      PhosphorIcons.camera(),
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ),
-                title: Text(
-                  'Câmera',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                onTap: () async {
-                  Navigator.of(context).pop();
-                  //Tirar foto da câmera
-                },
-              ),
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[200],
-                  child: Center(
-                    child: Icon(
-                      PhosphorIcons.trash(),
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ),
-                title: Text(
-                  'Remover',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  //Torna a foto Null
-                  setState(() {
-                    imageFile = null;
-                  });
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
 
   @override
@@ -171,46 +53,10 @@ class _CadastrarProdutosPageState extends State<CadastrarProdutosPage> {
     return Scaffold(
       body: Center(
         child: Container(
-          // padding: EdgeInsets.all(50),
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Stack(
-                    children: [
-                      CircleAvatar(
-                        radius: 75,
-                        backgroundColor: Colors.grey[200],
-                        child: CircleAvatar(
-                          radius: 65,
-                          backgroundColor: Colors.grey[300],
-                          backgroundImage:
-                              imageFile != null ? FileImage(imageFile!) : null,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 5,
-                        right: 5,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.grey[200],
-                          child: IconButton(
-                            onPressed: _ShowOpcoesBottomSheet,
-                            icon: Icon(
-                              PhosphorIcons.pencilSimple(),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 15,
-              ),
               TextField(
                   controller: txtDescricao,
                   decoration: InputDecoration(
@@ -239,23 +85,25 @@ class _CadastrarProdutosPageState extends State<CadastrarProdutosPage> {
                 ),
               ),
               SizedBox(height: 15),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  if (randomNumbers.isNotEmpty)
-                    BarcodeWidget(
-                      data: randomNumbers,
-                      barcode: Barcode.code128(),
-                      width: 200,
-                      height: 100,
-                    )
-                ],
-              ),
-              SizedBox(height: 15),
+              // Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: <Widget>[
+              //     if (randomNumbers.isNotEmpty)
+              //       BarcodeWidget(
+              //         data: randomNumbers,
+              //         barcode: Barcode.code128(),
+              //         width: 200,
+              //         height: 100,
+              //       )
+              //   ],
+              // ),
               ElevatedButton(
                 onPressed: _generatedRandomNumber,
-                child: Text('Gerar Novo Código de barras'),
+                child: Text(randomNumbers.isEmpty
+                    ? 'Gerar Novo Código de barras'
+                    : 'Código de Barras: $randomNumbers'),
               ),
+              SizedBox(height: 15),
               Container(
                 width: MediaQuery.of(context).size.width,
                 child: ElevatedButton(
@@ -276,5 +124,76 @@ class _CadastrarProdutosPageState extends State<CadastrarProdutosPage> {
         ),
       ),
     ); // body é o restante em branco da tela
+
+    void _ShowOpcoesBottomSheet() {
+      showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return Padding(
+            padding: const EdgeInsets.all(14),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    child: Center(
+                      child: Icon(
+                        PhosphorIcons.download(PhosphorIconsStyle.regular),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    'Galeria',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    //Buscar a Imagem da galeria
+                  },
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    child: Center(
+                      child: Icon(
+                        PhosphorIcons.camera(),
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    'Câmera',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    //Tirar foto da câmera
+                  },
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    child: Center(
+                      child: Icon(
+                        PhosphorIcons.trash(),
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    'Remover',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
   }
 }
