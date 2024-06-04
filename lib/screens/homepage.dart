3b3b72f39c9240a4d24f250720bfe9d06b356a57
werikdestruidoRR <116517426+werikdestruidoRR@users.noreply.github.com>
2024-06-04 18:06:53 -0300

    Reenviando o layout da home atualizado

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -84,8 +84,8 @@ class _HomePage extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 218, 169, 8),
         title: Text(
-          "Bem Vindo",
-          style: TextStyle(color: Colors.white),
+          "Seja Bem Vindo !",
+          style: TextStyle(color: Colors.white, fontSize: 30),
         ),
         centerTitle: true,
       ),
@@ -97,7 +97,7 @@ class _HomePage extends State<HomePage> {
               child: Container(
                 child: Image.asset(
                   "lib/images/icon.png",
-                  width: 220,
+                  width: 320,
                 ),
               ),
             ),
@@ -142,7 +142,7 @@ class _HomePage extends State<HomePage> {
                     style: StylesProntos.textBotao(context, '20', Colors.white),
                   ),
                 ),
-                SizedBox(height: 30),
+                SizedBox(height: 100),
               ],
             ),
           ), // Aqui estava faltando o fechamento da chave
